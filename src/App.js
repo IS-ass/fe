8f32d8a7c88e359759d16a6bac31f3b09c2cbc6b
thuutri <thuutri2710@gmail.com>
2021-04-07 01:11:38 +0700

    feat: update logic

--- src/App.js
+++ src/App.js
@@ -8,47 +8,6 @@ const { Item } = Form;
 const { Content } = Layout;
 
 function App() {
-  const [check1, setCheck1] = useState(false);
-  const [check2, setCheck2] = useState(false);
-  const [check3, setCheck3] = useState(false);
-  const [check4, setCheck4] = useState(false);
-  const [check5, setCheck5] = useState(false);
-  const [check6, setCheck6] = useState(false);
-  const [check7, setCheck7] = useState(false);
-  const [check8, setCheck8] = useState(false);
-  const [check9, setCheck9] = useState(false);
-  const [check10, setCheck10] = useState(false);
-  const [check11, setCheck11] = useState(false);
-  const [check12, setCheck12] = useState(false);
-  const [check13, setCheck13] = useState(false);
-  const [check14, setCheck14] = useState(false);
-  const [check15, setCheck15] = useState(false);
-  const [check16, setCheck16] = useState(false);
-  const [check17, setCheck17] = useState(false);
-  const [check18, setCheck18] = useState(false);
-  const [check19, setCheck19] = useState(false);
-  const [check20, setCheck20] = useState(false);
-  const [check21, setCheck21] = useState(false);
-  const [check22, setCheck22] = useState(false);
-  const [check23, setCheck23] = useState(false);
-  const [check24, setCheck24] = useState(false);
-  const [check25, setCheck25] = useState(false);
-  const [check26, setCheck26] = useState(false);
-  const [check27, setCheck27] = useState(false);
-  const [check28, setCheck28] = useState(false);
-  const [check29, setCheck29] = useState(false);
-  const [check30, setCheck30] = useState(false);
-  const [check31, setCheck31] = useState(false);
-  const [check32, setCheck32] = useState(false);
-  const [check33, setCheck33] = useState(false);
-  const [check34, setCheck34] = useState(false);
-  const [check35, setCheck35] = useState(false);
-  const [check36, setCheck36] = useState(false);
-  const [check37, setCheck37] = useState(false);
-  const [check38, setCheck38] = useState(false);
-  const [check39, setCheck39] = useState(false);
-  const [check40, setCheck40] = useState(false);
-  const [check41, setCheck41] = useState(false);
   const [result, setResult] = useState("");
   const [isModalVisible, setIsModalVisible] = useState(false);
 
@@ -77,474 +36,50 @@ function App() {
         className="pt-10 px-8 w-1/2 m-auto bg-white h-full"
         formLayout="vertical"
       >
-        <Checkbox
-          onChange={(e) => setCheck1(e.target.checked)}
-          className="px-2"
-        >
-          Ngứa
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck2(value.target.checked)}
-          className="px-2"
-        >
-          Phát ban da
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck3(value.target.checked)}
-          className="px-2"
-        >
-          Nổi mụn trên da thành sần
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck4(value.target.checked)}
-          className="px-2"
-        >
-          Hắt xì hơi liên tục
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck5(value.target.checked)}
-          className="px-2"
-        >
-          Dị ứng
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck6(value.target.checked)}
-          className="px-2"
-        >
-          Viêm khớp
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck7(value.target.checked)}
-          className="px-2"
-        >
-          Hen phế quản
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck8(value.target.checked)}
-          className="px-2"
-        >
-          Đau dạ dáy
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck9(value.target.checked)}
-          className="px-2"
-        >
-          Thừa acid dạ dày
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck10(value.target.checked)}
-          className="px-2"
-        >
-          Loét lưỡi
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck11(value.target.checked)}
-          className="px-2"
-        >
-          Giãn cơ
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck12(value.target.checked)}
-          className="px-2"
-        >
-          Nôn mửa
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck13(value.target.checked)}
-          className="px-2"
-        >
-          Đi tiểu rát
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck14(value.target.checked)}
-          className="px-2"
-        >
-          Đái ra máu
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck15(value.target.checked)}
-          className="px-2"
-        >
-          Mệt mỏi
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck16(value.target.checked)}
-          className="px-2"
-        >
-          Nhiễm trùng nấm{" "}
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck17(value.target.checked)}
-          className="px-2"
-        >
-          Bệnh trào ngược dạ dày thực quản
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck18(value.target.checked)}
-          className="px-2"
-        >
+        <Checkbox className="px-2">Ngứa</Checkbox>
+        <Checkbox className="px-2">Phát ban da</Checkbox>
+        <Checkbox className="px-2">Nổi mụn trên da thành sần</Checkbox>
+        <Checkbox className="px-2">Hắt xì hơi liên tục</Checkbox>
+        <Checkbox className="px-2">Dị ứng</Checkbox>
+        <Checkbox className="px-2">Viêm khớp</Checkbox>
+        <Checkbox className="px-2">Hen phế quản</Checkbox>
+        <Checkbox className="px-2">Đau dạ dáy</Checkbox>
+        <Checkbox className="px-2">Thừa acid dạ dày</Checkbox>
+        <Checkbox className="px-2">Loét lưỡi</Checkbox>
+        <Checkbox className="px-2">Giãn cơ</Checkbox>
+        <Checkbox className="px-2">Nôn mửa</Checkbox>
+        <Checkbox className="px-2">Đi tiểu rát</Checkbox>
+        <Checkbox className="px-2">Đái ra máu</Checkbox>
+        <Checkbox className="px-2">Mệt mỏi</Checkbox>
+        <Checkbox className="px-2">Nhiễm trùng nấm </Checkbox>
+        <Checkbox className="px-2">Bệnh trào ngược dạ dày thực quản</Checkbox>
+        <Checkbox className="px-2">
           Viêm dạ dày ruột hoặc tiêu chảy nhiễm trùng
         </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck19(value.target.checked)}
-          className="px-2"
-        >
-          Đột quỵ
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck20(value.target.checked)}
-          className="px-2"
-        >
-          Viêm gan B
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck21(value.target.checked)}
-          className="px-2"
-        >
-          Viêm gan C
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck22(value.target.checked)}
-          className="px-2"
-        >
-          Viêm gan D
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck23(value.target.checked)}
-          className="px-2"
-        >
-          Viêm gan E
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck24(value.target.checked)}
-          className="px-2"
-        >
-          Cao huyết áp
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck25(value.target.checked)}
-          className="px-2"
-        >
-          Cường giáp
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck26(value.target.checked)}
-          className="px-2"
-        >
-          Hạ đường huyết
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck27(value.target.checked)}
-          className="px-2"
-        >
-          Suy giáp
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck28(value.target.checked)}
-          className="px-2"
-        >
-          Bệnh chốc lở
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck29(value.target.checked)}
-          className="px-2"
-        >
-          Vàng da
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck30(value.target.checked)}
-          className="px-2"
-        >
-          Sốt rét
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck31(value.target.checked)}
-          className="px-2"
-        >
-          Đau nửa đầu
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck32(value.target.checked)}
-          className="px-2"
-        >
-          Viêm khớp
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck33(value.target.checked)}
-          className="px-2"
-        >
-          Tê liệt (xuất huyết não)
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck34(value.target.checked)}
-          className="px-2"
-        >
-          Loét dạ dày tá tràng
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck35(value.target.checked)}
-          className="px-2"
-        >
-          Phê cầu khuẩn
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck36(value.target.checked)}
-          className="px-2"
-        >
-          Vảy nến
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck37(value.target.checked)}
-          className="px-2"
-        >
-          Lao
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck38(value.target.checked)}
-          className="px-2"
-        >
-          Thương hàn{" "}
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck39(value.target.checked)}
-          className="px-2"
-        >
-          Nhiễm trùng đường tiểu
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck40(value.target.checked)}
-          className="px-2"
-        >
-          Giãn tĩnh mạch
-        </Checkbox>
-        <Checkbox
-          onChange={(value) => setCheck41(value.target.checked)}
-          className="px-2"
-        >
-          Viêm gan A
-        </Checkbox>
-        <Form
-          name="symptom"
-          className="flex flex-wrap -mx-4 mt-10"
-          layout="vertical"
-          onFinish={onSubmit}
-        >
-          {check1 ? (
-            <Item className="w-1/2" label="Ngứa">
-              <Input />
-            </Item>
-          ) : null}
-          {check2 ? (
-            <Item className="w-1/2" label="Phát ban da">
-              <Input />
-            </Item>
-          ) : null}
-          {check3 ? (
-            <Item className="w-1/2" label="Nổi mụn trên da thành sần">
-              <Input />
-            </Item>
-          ) : null}
-          {check4 ? (
-            <Item className="w-1/2" label="Hắt xì hơi liên tục">
-              <Input />
-            </Item>
-          ) : null}
-          {check5 ? (
-            <Item className="w-1/2" label="Lạnh cóng">
-              <Input />
-            </Item>
-          ) : null}
+        <Checkbox className="px-2">Đột quỵ</Checkbox>
+        <Checkbox className="px-2">Viêm gan B</Checkbox>
+        <Checkbox className="px-2">Viêm gan C</Checkbox>
+        <Checkbox className="px-2">Viêm gan D</Checkbox>
+        <Checkbox className="px-2">Viêm gan E</Checkbox>
+        <Checkbox className="px-2">Cao huyết áp</Checkbox>
+        <Checkbox className="px-2">Cường giáp</Checkbox>
+        <Checkbox className="px-2">Hạ đường huyết</Checkbox>
+        <Checkbox className="px-2">Suy giáp</Checkbox>
+        <Checkbox className="px-2">Bệnh chốc lở</Checkbox>
+        <Checkbox className="px-2">Vàng da</Checkbox>
+        <Checkbox className="px-2">Sốt rét</Checkbox>
+        <Checkbox className="px-2">Đau nửa đầu</Checkbox>
+        <Checkbox className="px-2">Viêm khớp</Checkbox>
+        <Checkbox className="px-2">Tê liệt (xuất huyết não)</Checkbox>
+        <Checkbox className="px-2">Loét dạ dày tá tràng</Checkbox>
+        <Checkbox className="px-2">Phê cầu khuẩn</Checkbox>
+        <Checkbox className="px-2">Vảy nến</Checkbox>
+        <Checkbox className="px-2">Lao</Checkbox>
+        <Checkbox className="px-2">Thương hàn </Checkbox>
+        <Checkbox className="px-2">Nhiễm trùng đường tiểu</Checkbox>
+        <Checkbox className="px-2">Giãn tĩnh mạch</Checkbox>
+        <Checkbox className="px-2">Viêm gan A</Checkbox>
 
-          {check6 ? (
-            <Item className="w-1/2" label="Ớn lạnh">
-              <Input />
-            </Item>
-          ) : null}
-          {check7 ? (
-            <Item className="w-1/2" label="Đau khớp">
-              <Input />
-            </Item>
-          ) : null}
-          {check8 ? (
-            <Item className="w-1/2" label="Đau dạ dáy">
-              <Input />
-            </Item>
-          ) : null}
-          {check9 ? (
-            <Item className="w-1/2" label="Thừa acid dạ dày">
-              <Input />
-            </Item>
-          ) : null}
-          {check10 ? (
-            <Item className="w-1/2" label="Loét lưỡi">
-              <Input />
-            </Item>
-          ) : null}
-          {check11 ? (
-            <Item className="w-1/2" label="Giãn cơ">
-              <Input />
-            </Item>
-          ) : null}
-          {check12 ? (
-            <Item className="w-1/2" label="Nôn mửa">
-              <Input />
-            </Item>
-          ) : null}
-          {check13 ? (
-            <Item className="w-1/2" label="Đi tiểu rát">
-              <Input />
-            </Item>
-          ) : null}
-          {check14 ? (
-            <Item className="w-1/2" label="Đái ra máu">
-              <Input />
-            </Item>
-          ) : null}
-          {check15 ? (
-            <Item className="w-1/2" label="Mệt mỏi">
-              <Input />
-            </Item>
-          ) : null}
-          {check16 ? (
-            <Item className="w-1/2" label="Nhiễm trùng nấm">
-              <Input />
-            </Item>
-          ) : null}
-          {check17 ? (
-            <Item className="w-1/2" label="Bệnh trào ngược dạ dày thực quản">
-              <Input />
-            </Item>
-          ) : null}
-          {check18 ? (
-            <Item
-              className="w-1/2"
-              label="Viêm dạ dày ruột hoặc tiêu chảy nhiễm trùng"
-            >
-              <Input />
-            </Item>
-          ) : null}
-          {check19 ? (
-            <Item className="w-1/2" label="Đột quỵ">
-              <Input />
-            </Item>
-          ) : null}
-          {check20 ? (
-            <Item className="w-1/2" label="Viêm gan B">
-              <Input />
-            </Item>
-          ) : null}
-          {check21 ? (
-            <Item className="w-1/2" label="Viêm gan C">
-              <Input />
-            </Item>
-          ) : null}
-          {check22 ? (
-            <Item className="w-1/2" label="Viêm gan D">
-              <Input />
-            </Item>
-          ) : null}
-          {check23 ? (
-            <Item className="w-1/2" label="Viêm gan E">
-              <Input />
-            </Item>
-          ) : null}
-          {check24 ? (
-            <Item className="w-1/2" label="Cao huyết áp">
-              <Input />
-            </Item>
-          ) : null}
-          {check25 ? (
-            <Item className="w-1/2" label="Cường giáp">
-              <Input />
-            </Item>
-          ) : null}
-          {check26 ? (
-            <Item className="w-1/2" label="Hạ đường huyết">
-              <Input />
-            </Item>
-          ) : null}
-          {check27 ? (
-            <Item className="w-1/2" label="Suy giáp">
-              <Input />
-            </Item>
-          ) : null}
-          {check28 ? (
-            <Item className="w-1/2" label="Bệnh chốc lở">
-              <Input />
-            </Item>
-          ) : null}
-          {check29 ? (
-            <Item className="w-1/2" label="Vàng da">
-              <Input />
-            </Item>
-          ) : null}
-          {check30 ? (
-            <Item className="w-1/2" label="Sốt rét">
-              <Input />
-            </Item>
-          ) : null}
-          {check31 ? (
-            <Item className="w-1/2" label="Đau nửa đầu">
-              <Input />
-            </Item>
-          ) : null}
-          {check32 ? (
-            <Item className="w-1/2" label="Viêm khơp">
-              <Input />
-            </Item>
-          ) : null}
-          {check33 ? (
-            <Item className="w-1/2" label="Tê liệt (xuất huyết não)">
-              <Input />
-            </Item>
-          ) : null}
-          {check34 ? (
-            <Item className="w-1/2" label="Loét dạ dày tá tràng">
-              <Input />
-            </Item>
-          ) : null}
-          {check35 ? (
-            <Item className="w-1/2" label="Phế cầu khuẩn">
-              <Input />
-            </Item>
-          ) : null}
-          {check36 ? (
-            <Item className="w-1/2" label="Vẩy nến">
-              <Input />
-            </Item>
-          ) : null}
-          {check37 ? (
-            <Item className="w-1/2" label="Lao">
-              <Input />
-            </Item>
-          ) : null}
-          {check38 ? (
-            <Item className="w-1/2" label="Thương hàn">
-              <Input />
-            </Item>
-          ) : null}
-          {check39 ? (
-            <Item className="w-1/2" label="Nhiễm trùng đường tiểu">
-              <Input />
-            </Item>
-          ) : null}
-          {check40 ? (
-            <Item className="w-1/2" label="Giãn tĩnh mạch">
-              <Input />
-            </Item>
-          ) : null}
-          {check41 ? (
-            <Item className="w-1/2" label="Viêm gan A">
-              <Input />
-            </Item>
-          ) : null}
-
-          <Item className="w-full">
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Item>
-        </Form>
         <Modal
           title="Basic Modal"
           visible={isModalVisible}
