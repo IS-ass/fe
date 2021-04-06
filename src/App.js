9d0279d7be93ebe24b3f11b090d1132376fd3416
thuutri <thuutri2710@gmail.com>
2021-04-06 23:51:42 +0700

    feat: build UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,291 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import "./App.css";
+import { Form, Checkbox, Layout, Input } from "antd";
+import { useState } from "react";
+
+const { Item } = Form;
+const { Content } = Layout;
 
 function App() {
+  const [check1, setCheck1] = useState(false);
+  const [check2, setCheck2] = useState(false);
+  const [check3, setCheck3] = useState(false);
+  const [check4, setCheck4] = useState(false);
+  const [check5, setCheck5] = useState(false);
+  const [check6, setCheck6] = useState(false);
+  const [check7, setCheck7] = useState(false);
+  const [check8, setCheck8] = useState(false);
+  const [check9, setCheck9] = useState(false);
+  const [check10, setCheck10] = useState(false);
+  const [check11, setCheck11] = useState(false);
+  const [check12, setCheck12] = useState(false);
+  const [check13, setCheck13] = useState(false);
+  const [check14, setCheck14] = useState(false);
+  const [check15, setCheck15] = useState(false);
+  const [check16, setCheck16] = useState(false);
+  const [check17, setCheck17] = useState(false);
+  const [check18, setCheck18] = useState(false);
+  const [check19, setCheck19] = useState(false);
+  const [check20, setCheck20] = useState(false);
+  const [check21, setCheck21] = useState(false);
+  const [check22, setCheck22] = useState(false);
+  const [check23, setCheck23] = useState(false);
+  const [check24, setCheck24] = useState(false);
+  const [check25, setCheck25] = useState(false);
+  const [check26, setCheck26] = useState(false);
+  const [check27, setCheck27] = useState(false);
+  const [check28, setCheck28] = useState(false);
+  const [check29, setCheck29] = useState(false);
+  const [check30, setCheck30] = useState(false);
+  const [check31, setCheck31] = useState(false);
+  const [check32, setCheck32] = useState(false);
+  const [check33, setCheck33] = useState(false);
+  const [check34, setCheck34] = useState(false);
+  const [check35, setCheck35] = useState(false);
+  const [check36, setCheck36] = useState(false);
+  const [check37, setCheck37] = useState(false);
+  const [check38, setCheck38] = useState(false);
+  const [check39, setCheck39] = useState(false);
+  const [check40, setCheck40] = useState(false);
+  const [check41, setCheck41] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-50 h-screen pt-10">
+      <Content
+        className="pt-10 px-8 w-1/2 m-auto bg-white h-full"
+        formLayout="vertical"
+      >
+        <Checkbox onChange={(value) => setCheck1(value)} className="px-2">
+          Chóng mặt lành tính do tư thế
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck2(value)} className="px-2">
+          AIDS
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck3(value)} className="px-2">
+          Nổi mụn
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck4(value)} className="px-2">
+          Gan nhiễm mỡ không do rượu
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck5(value)} className="px-2">
+          Dị ứng
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck6(value)} className="px-2">
+          Viêm khớp
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck7(value)} className="px-2">
+          Hen phế quản
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck8(value)} className="px-2">
+          THoái hoá cột sống cổ
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck9(value)} className="px-2">
+          Thuỷ đậu
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck10(value)} className="px-2">
+          Ứ mật mãn tính
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck11(value)} className="px-2">
+          Cảm thông thường
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck12(value)} className="px-2">
+          Sốt xuất huyết
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck13(value)} className="px-2">
+          Béo phì
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck14(value)} className="px-2">
+          Trĩ hỗn hợp
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck15(value)} className="px-2">
+          Dị ứng thuốc
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck16(value)} className="px-2">
+          Nhiễm trùng nấm{" "}
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck17(value)} className="px-2">
+          Bệnh trào ngược dạ dày thực quản
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck18(value)} className="px-2">
+          Viêm dạ dày ruột hoặc tiêu chảy nhiễm trùng
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck19(value)} className="px-2">
+          Đột quỵ
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck20(value)} className="px-2">
+          Viêm gan B
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck21(value)} className="px-2">
+          Viêm gan C
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck22(value)} className="px-2">
+          Viêm gan D
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck23(value)} className="px-2">
+          Viêm gan E
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck24(value)} className="px-2">
+          Cao huyết áp
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck25(value)} className="px-2">
+          Cường giáp
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck26(value)} className="px-2">
+          Hạ đường huyết
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck27(value)} className="px-2">
+          Suy giáp
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck28(value)} className="px-2">
+          Bệnh chốc lở
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck29(value)} className="px-2">
+          Vàng da
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck30(value)} className="px-2">
+          Sốt rét
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck31(value)} className="px-2">
+          Đau nửa đầu
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck32(value)} className="px-2">
+          Viêm khớp
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck33(value)} className="px-2">
+          Tê liệt (xuất huyết não)
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck34(value)} className="px-2">
+          Loét dạ dày tá tràng
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck35(value)} className="px-2">
+          Phê cầu khuẩn
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck36(value)} className="px-2">
+          Vảy nến
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck37(value)} className="px-2">
+          Lao
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck38(value)} className="px-2">
+          Thương hàn{" "}
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck39(value)} className="px-2">
+          Nhiễm trùng đường tiểu
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck40(value)} className="px-2">
+          Giãn tĩnh mạch
+        </Checkbox>
+        <Checkbox onChange={(value) => setCheck41(value)} className="px-2">
+          Viêm gan A
+        </Checkbox>
+        <Form className="flex flex-wrap -mx-4 mt-10" layout="vertical">
+          {check1 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check2 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check3 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check4 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check5 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check6 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check7 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check8 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check9 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check10 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check11 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check12 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check13 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check14 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check15 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check16 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check17 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check18 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check19 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check20 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+          {check21 ? (
+            <Item className="w-1/2" label="Chóng mặt lành tính do tư thế">
+              <Input />
+            </Item>
+          ) : null}
+        </Form>
+      </Content>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  important: true,
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    // ...
+  },
+  // ...
+};
